5cde9d5e8c01140f9085fe6fddd9912f02dde915
Paulo Ariel Pareja <paulo.pareja@hotmail.es>
2022-03-02 20:13:42 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Button } from "react-native";
+import { Map, Modal, Panel, Input, List } from "./components";
 
 export default function App() {
+  const [puntos, setPuntos] = useState([]);
+  const [puntoTemp, setPuntoTemp] = useState({});
+  const [nombrePunto, setNombrePunto] = useState("");
+  const [visibility, setVisibility] = useState(false);
+  const [pointsFilter, setPointsFilter] = useState(true);
+  const [visibilityFilter, setVisibilityFilter] = useState("new_punto");
+
+
+  const togglePointsFilter = () =>  setPointsFilter(!pointsFilter)
+  
+  const handleLongPress = ({ nativeEvent }) => {
+    setVisibilityFilter("new_punto");
+    setPuntoTemp(nativeEvent.coordinate);
+    setVisibility(true);
+  };
+
+  const handleChangeText = (text) => {
+    setNombrePunto(text);
+  };
+
+  const handleSubmit = () => {
+    const newPunto = {
+      coordinate: puntoTemp,
+      name: nombrePunto,
+    };
+    setPuntos(puntos.concat(newPunto));
+    setVisibility(false);
+    setPuntoTemp("");
+  };
+
+  const handleLista = () => {
+    setVisibilityFilter("all_puntos");
+    setVisibility(true);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Map onLongPress={handleLongPress} puntos={puntos} pointsFilter={pointsFilter}/>
+      <Panel onPressLeft={handleLista} textLeft={"Lista"} togglePointsFilter={togglePointsFilter}/>
+      <Modal visibility={visibility}>
+        {visibilityFilter === "new_punto" ? (
+          <View style={styles.form}>
+            <Input
+              title="Nombre"
+              placeholder="Nombre del punto"
+              onChangeText={handleChangeText}
+            />
+            <Button title="Agregar" onPress={handleSubmit} />
+          </View>
+        ) : (
+          <List puntos={puntos} closeModal={() => setVisibility(false)} />
+        )}
+      </Modal>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +65,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  form: {
+    padding: 20,
   },
 });
---
+++ README.md
@@ -0,0 +1,45 @@
+# React Native Mapa de Sitios Favoritos
+
+
+## Tabla de contenidos
+
+- [Acerca del proyecto](#about)
+- [Comencemos](#getting_started)
+
+## Acerca del proyecto <a name = "about"></a>
+
+El proyecto es un mapa donde se podra guardar puntos de interes en un mapa, luego se podra visualizar el listado y quitar/ver puntos colocados en el mapa.
+Esta realizado como ejercicio conceptual simple.
+<p>
+El proyecto fue realizado con Expo cli
+
+## Comencemos <a name = "getting_started"></a>
+
+Clorar repositorio, instalar dependencias con Yarn y utilizar un emulador/dispositivo para ejecutar la aplicacion.
+
+### Prerequisitos
+
+- Yarn
+
+```
+npm install --global yarn
+```
+
+- Dispositivo o emulador ejecutandose
+
+### Instalacion y ejecucion
+
+Instalar dependencias
+
+```
+yarn install
+```
+
+Ejecutar el proyecto
+
+```
+yarn start
+```
+
+Se desplegara un listado de opciones para ejecutarlo en Ios o Android.
+Version web no admitida.
